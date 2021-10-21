46977c6dee0ced50bfe11d4ee02816cd2bb03a5c
darya425 <dasha425@mail.ru>
2021-10-21 23:53:03 +0200

    cleared of comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,36 +5,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReviewModule } from './modules/review/review.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-// const ormConfig = {
-//   type: 'mysql',
-//   host: 'eu-cdbr-west-01.cleardb.com',
-//   port: 3306,
-//   username: 'b3dc7a593ca714',
-//   password: '125af6f1',
-//   database: 'heroku_2edee81e18ba363',
-//   entities: ['dist/**/*.entity{.ts,.js}'],
-//   synchronize: true,
-// };
-
-console.log(process.env.DB_USER_NAME);
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'mysql',
-//       host: process.env.DB_HOST,
-//       port: 3306,
-//       username: process.env.DB_USER_NAME,
-//       password: process.env.DB_PASSWORD,
-//       database: process.env.DB_NAME,
-//       entities: ['dist/**/*.entity{.ts,.js}'],
-//       synchronize: true,
-//     }),
-//     ReviewModule,
-//   ],
-// controllers: [AppController],
-// providers: [AppService],
-// })
 @Module({
   imports: [
     ConfigModule.forRoot(),
