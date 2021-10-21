5eaf24631a655d187ac81c97dfb95dcd8b30268d
darya425 <dasha425@mail.ru>
2021-10-21 20:27:52 +0200

    added heroku

--- src/modules/review/services/review.service.ts
+++ src/modules/review/services/review.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
-// import { Repository } from 'typeorm';
 import { Review } from '../entities/review.entity';
 
 @Injectable()
